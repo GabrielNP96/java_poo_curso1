ec3d354c03e1aa4ef9975e5e33aa23ce893c3c11
GabrielNP96 <gabrielnp1996@gmail.com>
2024-12-16 23:38:40 -0300

    lendo dados

--- src/Leitura.java
+++ src/Leitura.java
@@ -5,7 +5,16 @@ public class Leitura {
         Scanner leitura = new Scanner(System.in);
         System.out.println("Digite o nome do Filme");
         String filme = leitura.nextLine();
-        System.out.printf(filme);
+
+        System.out.println("Ano de lançamento do filme: ");
+        int anoDeLancamento = leitura.nextInt();
+
+        System.out.println("Sua avaliação do filme: ");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println("Nome do Filme: " + filme);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Avaliação: " + avaliacao);
 
     }
 }
