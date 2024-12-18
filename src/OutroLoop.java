4b67113ef505305a4440ece27be9e5d68c6bb2fe
GabrielNP96 <gabrielnp1996@gmail.com>
2024-12-17 22:39:49 -0300

    Outras formas de iterar dados

---
+++ src/OutroLoop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OutroLoop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int totalDenotas = 0;
+
+        while (nota != -1) {
+            System.out.println("Digite sua avaliação do filme pu -1 para encerrar: " + mediaAvaliacao/totalDenotas);
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
+            totalDenotas++;
+        }
+
+        if (notas != -1) {
+
+        }
+    }
+}
