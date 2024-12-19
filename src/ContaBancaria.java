2fe286ef87228ecdd7f489f62ddd8e158ef5db26
gabrielNP96 <gabrielnp1996@gmail.com>
2024-12-19 12:29:52 -0300

    iniciando variaveis

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -2,26 +2,14 @@ import java.util.Scanner;
 
 public class ContaBancaria {
     public static void main(String[] args) {
+        String nome = "Tom Kirkman";
+        String tipoConta = "Corrente";
+        double saldo = 2500;
 
-        Scanner leitura = new Scanner(System.in);
-        System.out.println("Vamos Registrar sua conta!");
-
-        System.out.println("Digite seu nome:");
-        String nome = leitura.nextLine();
-        System.out.println("Agora preciso saber se sua conta é corrente ou pupança:\nDigite 1 para corrente e 2 para poupança");
-        int tipoConta = leitura.nextInt();
-        String correnteOuPoupanca = "str";
-
-        if (tipoConta == 1) {
-             correnteOuPoupanca = "Corrente";
-        } else if (tipoConta == 2) {
-             correnteOuPoupanca = "Poupança";
-        } else {
-            System.out.println("Erro, tipo de conta invalido");
-        }
-        System.out.println("Qual valor incial da sua conta?");
-        double saldo = leitura.nextDouble();
-
-        System.out.println("Nome: " + nome + "\ntipo de conta: " + correnteOuPoupanca  + "\nSaldo: " + saldo);
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n***********************");
     }
 }
